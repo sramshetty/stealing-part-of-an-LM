992acd6a01d086752575e70ed7411e0bdcbb09cc
Shivaen <shivaen.ramshetty@gmail.com>
2024-03-15 22:51:56 -0700

    initial full logit methods

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # Stealing Part of a Language Model
 An unofficial implementation of ["Stealing Part of a Production Language Model"](https://arxiv.org/abs/2403.06634)
+
+### Details
+Attack reimplementations are for research and model safety/defense purposes alone.
+
+Llama 2 7b:
+    - Recover hidden dim $\pm 1$: 4095
+    - Predict RMSNorm as normalization layer
+    - Last layer reconstructed with an RMS of $2 * 10^-5$
+
+### Techniques
+With All Logits Available:
+- [x] Recovering Hidden Dimensionality
+    - [x] Normalization Layer Prediction
+- [x] Full Final Layer Extraction
+
+With Top-K Logits and Logit-bias
+- [ ] Recover complete logit vector
+    - [ ] constrained logit-bias case
+
+Logprob-free
+- [ ] Recover complete logit vector
+
+
+### Citation
+```bibtex
+@misc{carlini2024stealing,
+    title={Stealing Part of a Production Language Model}, 
+    author={Nicholas Carlini and Daniel Paleka and Krishnamurthy Dj Dvijotham and Thomas Steinke and Jonathan Hayase and A. Feder Cooper and Katherine Lee and Matthew Jagielski and Milad Nasr and Arthur Conmy and Eric Wallace and David Rolnick and Florian Tramèr},
+    year={2024},
+    eprint={2403.06634},
+    archivePrefix={arXiv},
+    primaryClass={cs.CR}
+}
+```
--- attacks/attacks.py
+++ attacks/attacks.py
@@ -11,8 +11,25 @@ def h_dim_extraction(
     dataset,
     text_key,
     n,
-    batch_size=4
+    batch_size=4,
+    predict_norm=False
 ):
+    """
+    Args:
+        tokenizer: tokenizer to use for prompt encoding
+        model: model to find the hidden dimension of
+        dataset: dataset to use for random prompt sampling
+        text_key: key in dataset that corresponds to the prompts
+        n: the number of samples to produce logits for
+        batch_size (Optional): batch size for model inference
+        predict_norm (Optional): whehther to predict which normalization layer type is used
+    
+    Returns:
+        u: unitary matrix
+        s: singular values
+        s_dim: log of the absolute singular values
+        count: predicted hidden dimension size
+    """
     l = model(torch.tensor([[0]]).to("cuda"), 0).size(-1)
     q = torch.zeros(n, l)
 
@@ -45,13 +62,49 @@ def h_dim_extraction(
         q[i * batch_size:i * (batch_size + 1)] = logits.to("cpu")
     
     # compute singular values and prepare them to find the multiplicative gap
-    s = torch.log(torch.linalg.svdvals(q.float()).abs() + 1e-9)  # avoid -inf
-    
+    u, s, _ = torch.linalg.svd(q.T.to(torch.float64), full_matrices=False)
+    s_dim = torch.log(s.abs())
+
     # avoid large drops in negative singular values from causing a larger h_dim to be predicted
     # do so by multiplying by the sign of the first number -> multiplicative gap remains negative
+    # also the last singular value is 0 so avoid using it for argmax computation
     count = torch.argmax(
-        torch.where(s[:-1] >= 0, 1, -1) * (s[:-1] - s[1:])
+        torch.where(s_dim[:-2] >= 0, 1, -1) * (s_dim[:-2] - s_dim[1:-1])
     ).item() + 1
+    
+    # TODO: Test with other models
+    if predict_norm:
+        # Detailed in appendix B.2.2
+        q = q.to(torch.float16)
+        q_sub = q - q.mean(dim=0)
+        del q
+
+        s_sub = torch.linalg.svdvals(q_sub.T.to(torch.float64))
+        s_sub_dim = torch.log(s_sub.abs())
+        count_sub =  torch.argmax(
+            torch.where(s_sub_dim[:-2] >= 0, 1, -1) * (s_sub_dim[:-2] - s_sub_dim[1:-1])
+        ).item() + 1
+
+        print("Model uses LayerNorm") if count_sub == count - 1 else print("Model uses RMSNorm")
 
     print(f"Hidden Dim: {count}")
-    return count, s
+    return u, s, s_dim, count
+
+
+def layer_extraction(w, u, s, h_dim):
+    """
+    Args:
+        w: model's actual weight matrix for last layer
+        u: unitary matrix computed from `h_dim_extraction`
+        s: singular values computed from `h_dim_extraction`
+        h_dim: model's actual hidden dimension
+
+    Returns:
+        pred_w: predicted w
+        g: affine transformation such that pred_w@g ~ w
+    """
+    w, u, s = w.to("cuda"), u[:, :h_dim].to("cuda"), s[:h_dim].to("cuda")
+    pred_w = u @ torch.diag(s)
+    g = torch.linalg.lstsq(pred_w, w.to(torch.float64)).solution
+
+    return pred_w, g
